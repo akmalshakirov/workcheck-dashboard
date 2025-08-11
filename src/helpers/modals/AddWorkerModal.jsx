819d66182ed5e666ffa4cdec7ccc5e0032fcdbd9
akmal <akmalpubg221@gmail.com>
2025-08-11 16:43:17 +0500

    ..

--- src/helpers/modals/AddWorkerModal.jsx
+++ src/helpers/modals/AddWorkerModal.jsx
@@ -2,4 +2,4 @@ import { Modal } from "../../components/ui/Modal/Modal";
 
 export const AddWorkerModal = ({ worker, isVisible }) => {
     return <>{isVisible && <Modal visible={isVisible}>s</Modal>}</>;
-};
+}
