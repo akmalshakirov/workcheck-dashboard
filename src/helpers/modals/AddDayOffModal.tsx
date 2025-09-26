f412f3a69a9ced139aafd4fea6afba8a84580497
akmalshakirov <akmalpubg221@gmail.com>
2025-09-26 20:59:25 +0500

    'Add day off' added ✅

--- src/helpers/modals/AddDayOffModal.tsx
+++ src/helpers/modals/AddDayOffModal.tsx
@@ -1,4 +1,5 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
@@ -19,24 +20,25 @@ const AddDayOffModal = ({
 }: AddDayOffProps) => {
     const { t } = useTranslation();
     const [addDayOffLoading, setDayOffLoading] = useState<boolean>(false);
-    const [dayOffData, setDayOffData] = useState<string>("");
-    const [selectedDate, setSelectedDate] = useState<string[]>([]);
+    const [selectedDate, setSelectedDate] = useState<string>("");
+    const [dates, setDates] = useState<string[]>([]);
+    const [dayOffName, setDayOffName] = useState<string>("");
     const token = localStorage.getItem("token");
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         setDayOffLoading(true);
-        const formData = new FormData(e.target as HTMLFormElement);
+        const formData = new FormData();
+        formData.append("name", dayOffName);
+        dates.map((d) => formData.append("dates", d));
+
         try {
-            const response = await axios.post<AxiosResponse>(
-                `${baseURL}/day-off/create`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                    formData,
-                }
-            );
+            const response = await axios.post(`${baseURL}/day-off/create`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                formData,
+            });
             if (response.status === 201) {
                 toast.success(response.data as any);
             }
@@ -47,9 +49,29 @@ const AddDayOffModal = ({
         }
     };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSelectedDate([e.target.value]);
-        console.log(e.target.value);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const val = e.target.value;
+        if (!val) {
+            setSelectedDate("");
+            return;
+        }
+
+        setSelectedDate(e.target.value);
+        setDates((prev) => {
+            const exists = prev.includes(val);
+            const next = exists
+                ? prev.filter((d) => d !== val)
+                : [...prev, val];
+            return next.slice().sort();
+        });
+    };
+
+    const handleRemoveDate = (d: string) => {
+        setDates((prev) => prev.filter((x) => x !== d));
+    };
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setDayOffName(e.target.value);
     };
 
     return (
@@ -67,6 +89,8 @@ const AddDayOffModal = ({
                         aria-required
                         disabled={addDayOffLoading}
                         title={t("add_day_off_name")}
+                        value={dayOffName}
+                        onChange={handleInputChange}
                     />
                 </label>
                 <label className='flex flex-col gap-2'>
@@ -76,20 +100,26 @@ const AddDayOffModal = ({
                         onChange={handleChange}
                         type='date'
                         name='dates'
+                        disabled={addDayOffLoading}
                         className='border border-gray-500/70 rounded-lg outline-none focus:ring-blue-400 focus:ring-1 p-2 transition disabled:opacity-50'
+                        required
+                        aria-required
                     />
                 </label>
-                {selectedDate.map((d, index) => (
-                    <span
-                        key={index}
-                        className={`mt-2 w-max border rounded-lg p-2 border-blue-800/50 bg-blue-200 transition-all duration-150 ${
-                            selectedDate?.length > 0
-                                ? "opacity-100 pointer-events-auto"
-                                : "opacity-0 pointer-events-none"
-                        }`}>
-                        {d}
-                    </span>
-                ))}
+                <div className='flex items-center flex-wrap gap-3'>
+                    {dates.map((d, index) => (
+                        <div
+                            key={index}
+                            className='flex items-center gap-2 border rounded-lg px-2 py-1.5 border-blue-800/50 bg-blue-200'>
+                            {d}
+                            <button
+                                className='p-0.5 border rounded-lg border-blue-500 bg-blue-300 active:bg-blue-400 transition'
+                                onClick={() => handleRemoveDate(d)}>
+                                <X size={19} />
+                            </button>
+                        </div>
+                    ))}
+                </div>
                 <div className='flex justify-end gap-2 mt-5'>
                     {!addDayOffLoading && (
                         <Button
